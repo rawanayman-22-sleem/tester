38f16b9daf7afd817dd0b280bd676d625574d2b1
Rawan <rawanaymansleem@gmail.com>
2025-03-05 23:36:26 +0200

    التعديلات الأخيرة

---
+++ lib/secondfile.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class secondfile extends StatelessWidget {
+  const secondfile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
